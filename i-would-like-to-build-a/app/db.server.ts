0276e3eb9ead3f251ce9da799692edf2047f24ea
zeke-north-standard <ezekiel@northstandard.co>
2026-06-11 22:51:41 -0400

    Add files via upload

---
+++ i-would-like-to-build-a/app/db.server.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  // eslint-disable-next-line no-var
+  var prismaGlobal: PrismaClient | undefined;
+}
+
+const prisma = global.prismaGlobal ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  global.prismaGlobal = prisma;
+}
+
+export default prisma;
